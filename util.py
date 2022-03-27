3b0d50e133314389f4cde338878f414b21290064
Mariam05 <tomariam@gmail.com>
2022-03-26 20:20:28 -0400

    created files for yi vals

--- util.py
+++ util.py
@@ -13,3 +13,10 @@ def logging_setup():
 
 arrival = 1
 departure = 2
+
+
+def write_to_file(filename, data):
+    ''' A utility function that takes a filename and appends the data to the last line in the file. '''
+    with open(filename, "a") as reader:
+        reader.write(data)
+ 
