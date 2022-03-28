366b0c5a9532b8637a09b1825b5b12ca040a976e
Mariam05 <tomariam@gmail.com>
2022-03-28 09:33:38 -0400

    write data to csv for init phase

--- inspector.py
+++ inspector.py
@@ -2,6 +2,7 @@ from typing import List
 from buffer import *
 from util import *
 from random import randint
+import csv
 
 class Inspector():
     def __init__(self, id: int, buffers: List[Buffer]):
@@ -9,6 +10,7 @@ class Inspector():
         self.idle = True # is it inspecting a component
         self.buffers = buffers
         self.done = False # indicates whether the inspector finished inspecting all components (used only for this iteration)
+        self.processing_time = []
     
     def determine_target(self) -> Buffer:
         ''' Determine which buffer to add the component to '''
@@ -22,6 +24,26 @@ class Inspector():
         self.idle = True # set self to idle, so that it can start processing another component
         target_buffer = self.determine_target() # determine which buffer to place the component in
         target_buffer.add_to_buffer() # add the component to that buffer
+
+    def proportion_of_time_idle(self, clock):
+        service_time = 0; 
+        for t in self.processing_time:
+            service_time += t
+            
+        print("service time:  ", service_time, " Clock = " , clock)
+        idle_time = (clock - service_time)/clock
+        return idle_time
+    
+    def is_blocked(self) -> bool:
+        ''' Determine whether or not the inspector can start processing another component. 
+        An inspector can't process another component if it's blocked or busy'''
+        if not self.idle: # if it's currently processing a component, then return True 
+            return True
+
+        for buffer in self.buffers:
+            if not buffer.is_full(): # if there's space in a target buffer, return false
+                return False
+        return True
 
 class Inspector1(Inspector):
     def __init__(self, buffers: List[Buffer]):
@@ -46,19 +68,9 @@ class Inspector1(Inspector):
         if (insp_time == None):
             self.blocked = True
             return
+        self.processing_time.append(insp_time)
+        write_to_csv("quantities-data/insp1.csv",[insp_time, clock, clock+insp_time])
         return (insp_time + clock, arrival, None, 1, self.id)
-
-    def is_blocked(self) -> bool:
-        ''' Determine whether or not the inspector can start processing another component. 
-        An inspector can't process another component if it's blocked or busy'''
-        if not self.idle: # if it's currently processing a component, then return True 
-            return True
-
-        for buffer in self.buffers:
-            if not buffer.is_full(): # if there's space in a target buffer, return false
-                return False
-        return True
-
 
 class Inspector2(Inspector):
     def __init__(self, buffers: List[Buffer]):
@@ -99,14 +111,7 @@ class Inspector2(Inspector):
         if (insp_time == None):
             self.blocked = True
             return
-        return (insp_time + clock, arrival, None, 1, self.id)
+        self.processing_time.append(insp_time)
+        write_to_csv("quantities-data/insp2.csv",[insp_time, clock, clock+insp_time])
 
-    def is_blocked(self) -> bool:
-        ''' Determine whether or not the inspector can start processing another component.
-         An inspector can't process another component if it's blocked or busy '''
-        if not self.idle:
-            return True
-        for buffer in self.buffers:
-            if buffer.is_full():
-                return True
-        return False
+        return (insp_time + clock, arrival, None, 1, self.id)
